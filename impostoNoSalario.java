bfdbe280b11fe5fa0ac34a3ef081d59488b20636
m0rgan7 <161097407+m0rgan7@users.noreply.github.com>
2025-04-07 21:38:08 -0300

    Atividade
    mudei o nome das variáveis

--- impostoNoSalario.java
+++ impostoNoSalario.java
@@ -3,36 +3,36 @@ public class impostoNoSalario {
   public static void main (String[] args) {
      Scanner input = new Scanner(System.in);
     
-    float valorhr, ifr, valormes, salariobruto, inss;
+    float ValorHr, ImpostoDeRenda, ValorMes, SalarioBruto, inss;
       
     System.out.println("Digite o valor da hora");
-    valorhr = input.nextFloat();
+    ValorHr = input.nextFloat();
     System.out.println("Digite a quantidade de horas trabalhadas");
-    valormes = input.nextFloat();
+    ValorMes = input.nextFloat();
 
     salariobruto = valorhr* valormes;
     inss = 100*10/salariobruto;
 
-    if (salariobruto <=900 && salariobruto <= 1500) {
-      ifr = 100 * 5/salariobruto;
+    if (SalarioBruto <=900 && SalarioBruto <= 1500) {
+      ImpostoDeRenda = 100 * 5/SalarioBruto;
     }
-    else if (salariobruto <=2500){
-      ifr = 100*10/salariobruto;
+    else if (SalarioBruto <=2500){
+      ImpostoDeRenda = 100*10/SalarioBruto;
     }
     else {
-      ifr = 100*20/salariobruto;
+      ImpostoDeRenda = 100*20/SalarioBruto;
     }
     
-    float fgts, sind, salarioliquido;
-    fgts = 11*100/salariobruto;
-    sind = 3*100/salariobruto;
-    salarioliquido = salariobruto - ifr - fgts - sind - inss;
+    float fgts, sind, SalarioLiquido;
+    fgts = 11*100/SsalarioBruto;
+    sind = 3*100/SalarioBruto;
+    SalarioLiquido = SalarioBruto - ifr - fgts - sind - inss;
 
     System.out.println("Salário bruto: R$"+salariobruto);
     System.out.println("(-) IR: R$" +ifr);
     System.out.println("(-) INSS: R$" +inss);
     System.out.println("FGTS: R$"+fgts);
-    System.out.println("Salário liquido: R$"+salarioliquido);
+    System.out.println("Salário liquido: R$"+SalarioLiquido);
 
         input.close();
     }
