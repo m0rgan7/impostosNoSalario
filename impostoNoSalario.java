0437e0559b29c89b7e9d9629e6b79418d5be5ed8
Lavínia Melo <m0rgan7@users.noreply.github.com>
2026-01-05 20:57:31 -0300

    primeiro commit no pc novo

--- impostoNoSalario.java
+++ impostoNoSalario.java
@@ -7,31 +7,30 @@ public class impostoNoSalario {
       
     System.out.println("Digite o valor da hora");
     ValorHr = input.nextFloat();
-    System.out.println("Digite a quantidade de horas trabalhadas");
+    System.out.println("Digite a quantidade de horas trabalhadas no mês");
     ValorMes = input.nextFloat();
 
-    salariobruto = valorhr* valormes;
-    inss = 100*10/salariobruto;
+    SalarioBruto = ValorHr* ValorMes;
+    inss = 100*10/SalarioBruto;
 
     if (SalarioBruto <=900 && SalarioBruto <= 1500) {
-      ImpostoDeRenda = 100 * 5/SalarioBruto;
+      ImpostoDeRenda = 100 * (5/SalarioBruto);
     }
     else if (SalarioBruto <=2500){
-      ImpostoDeRenda = 100*10/SalarioBruto;
+      ImpostoDeRenda = 100*(10/SalarioBruto);
     }
     else {
-      ImpostoDeRenda = 100*20/SalarioBruto;
+      ImpostoDeRenda = 100*(20/SalarioBruto);
     }
     
-    float fgts, sind, SalarioLiquido;
-    fgts = 11*100/SsalarioBruto;
-    sind = 3*100/SalarioBruto;
-    SalarioLiquido = SalarioBruto - ifr - fgts - sind - inss;
+    float sind, SalarioLiquido;
+        sind = 3*100/SalarioBruto;
+    SalarioLiquido = SalarioBruto - ImpostoDeRenda - sind - inss;
 
-    System.out.println("Salário bruto: R$"+salariobruto);
-    System.out.println("(-) IR: R$" +ifr);
+    System.out.println("Salário bruto: R$"+SalarioBruto);
+    System.out.println("(-) IR: R$" +ImpostoDeRenda);
     System.out.println("(-) INSS: R$" +inss);
-    System.out.println("FGTS: R$"+fgts);
+    System.out.println("O valor do seu FGTS deve ser: R$");
     System.out.println("Salário liquido: R$"+SalarioLiquido);
 
         input.close();
