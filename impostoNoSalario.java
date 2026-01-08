72357e4c949671a37a51801c0f536d78ec275164
Lavínia Melo <161097407+m0rgan7@users.noreply.github.com>
2026-01-08 14:17:55 -0300

    Refactor salary calculation and user input handling
    remoção do desconto sindical. adição de prompts pro usuário informar o cargo ocupado e ajuste de lógica para o cálculo do FGTS e do imposto de renda.

--- impostoNoSalario.java
+++ impostoNoSalario.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
  * Programa que calcula o salário líquido de um funcionário Brasileiro
  * com base no salário bruto.
  * 
- * Descontos considerados: sinticato, inss, ir e fgts
+ * Descontos considerados: inss e imposto de renda
  * 
  * @author Morgan Melo
  * @version 1.0
@@ -25,17 +25,42 @@ public class impostoNoSalario {
      Scanner input = new Scanner(System.in);
 
     //Principais variaveis
-    float ImpostoDeRenda, SalarioBruto;
+    float ImpostoDeRenda, SalarioBruto, SalarioLiquido, fgts = 0;
       
     //Solicita o salário bruto ao usuário
     System.out.println("Digite seu salário bruto");
     SalarioBruto = input.nextFloat();
-
+    System.out.println("-----------------------------------------------");
+    
+    //Solicita o cargo do colaborador
+    System.out.println("Digite o numero correspondente com o seu cargo:");
+    System.out.println("1 - Trabalhador em geral");
+    System.out.println("2 - Trabalhador doméstico");
+    System.out.println("3 - Jovem aprendiz");
+    System.out.println("-----------------------------------------------");
+    int Colaboracao = input.nextInt();
+    //tem como fazer pro resultado digitado do imput nao ficar salvo no terminal?
+    System.out.println("-----------------------------------------------");
+    
+    //Faz o cálculo de acordo com o cargo do funcionário
+    if (Colaboracao == 1){
+        fgts = SalarioBruto * 0.08f;
+    }
+    else if (Colaboracao == 2){
+        fgts = SalarioBruto * 0.112f;
+    }
+     else if (Colaboracao == 3){
+        fgts = SalarioBruto * 0.02f;
+    }
+    //planejo utilizar aquele que nao tem que executar o codigo todo novamente
+    else {
+        System.out.println("Digite uma opção válida");
+    }
+    
     /*
     * Cálculo do imposto de renda de acordo com a faixa salárial
     */
-
-    if (SalarioBruto <=900 && SalarioBruto <= 1500) {
+    if (SalarioBruto <=900) {
       ImpostoDeRenda = (SalarioBruto * 5f) / 100;
     }
     else if (SalarioBruto <=2500){
@@ -45,22 +70,17 @@ public class impostoNoSalario {
       ImpostoDeRenda = SalarioBruto*0.2f;
     }
     
-    //Cálculo dos demais descontos
-    float sind, SalarioLiquido;
-    sind = SalarioBruto * 0.03f;
-    
     //Cálculo do salário recebido após descontos
-    SalarioLiquido = SalarioBruto - ImpostoDeRenda - sind;
+    SalarioLiquido = SalarioBruto - ImpostoDeRenda;
 
     // Exibição dos resultados
     System.out.println("Salário bruto: R$"+ SalarioBruto);
-    System.out.println("(-) IR: R$" + ImpostoDeRenda);
-    System.out.println("(-) INSS: R$");
-    System.out.println("O valor do seu FGTS deve ser: R$");
     System.out.println("Salário liquido: R$"+ SalarioLiquido);
-
-
-      // Fecha o Scanner para liberar recursos
-      input.close();
+    System.out.println("Valor descontado do Imposto de Renda: R$" + ImpostoDeRenda);
+    System.out.println("Valor descontado do INSS: R$");
+    System.out.println("O valor do seu FGTS deve ser: R$" + fgts);
+    
+    // Fecha o Scanner para liberar recursos
+    input.close();
     }
 } 
