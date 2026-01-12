68efdc7c1e6d191291756396d86998cd87edb045
Lavínia Melo <m0rgan7@users.noreply.github.com>
2026-01-12 19:35:20 -0300

    modified:   impostoNoSalario.java

--- impostoNoSalario.java
+++ impostoNoSalario.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-
+import java.util.InputMismatchException;
 /** 
  * Programa que calcula o salário líquido de um funcionário Brasileiro
  * com base no salário bruto.
@@ -11,7 +11,7 @@ import java.util.Scanner;
  * @since 06-01-2026
  **/
 
-public class impostoNoSalario {
+public class ImpostoNoSalario {
 
   /**
    * Método principal que executa o cálculo do salário líquido.
@@ -25,71 +25,77 @@ public class impostoNoSalario {
     Scanner input = new Scanner(System.in);
 
     //Principais variaveis
-    int Colaboracao;
-    float ImpostoDeRenda, SalarioBruto, SalarioLiquido, fgts = 0;
+    int colaboracao = 0;
+    float impostoDeRenda, salarioBruto, salarioLiquido, fgts = 0;
       
     //Solicita o salário bruto ao usuário
     System.out.println("Digite seu salário bruto");
-    SalarioBruto = input.nextFloat();
+    salarioBruto = input.nextFloat();
     System.out.println("-----------------------------------------------");
+    
+    do{ //so permite o codigo com um número válido
+    
+        //Trata exceção para entrada inválida
+        try {
 
-    //so permite o codigo com um número válido
-    do{
-    //Solicita o cargo do colaborador
-    System.out.println("Digite o numero correspondente com o seu cargo:");
-    System.out.println("1 - Trabalhador em geral");
-    System.out.println("2 - Trabalhador doméstico");
-    System.out.println("3 - Jovem aprendiz");
-    System.out.println("-----------------------------------------------");
-    Colaboracao = input.nextInt();
-    System.out.println("-----------------------------------------------");
-    
-    //Calcula o FGTS de acordo com o cargo do funcionário
-    if (Colaboracao == 1){
-        fgts = SalarioBruto * 0.08f;
-     }
-    else if (Colaboracao == 2){
-        fgts = SalarioBruto * 0.112f;
-     }
-     else if (Colaboracao == 3){
-        fgts = SalarioBruto * 0.02f;
-     }
-    else {
-        System.out.println("Digite uma opção válida");
-     }
+            //Solicita o cargo do colaborador
+            System.out.println("Digite o numero correspondente com o seu cargo:");
+            System.out.println("1 - Trabalhador em geral");
+            System.out.println("2 - Trabalhador doméstico");
+            System.out.println("3 - Jovem aprendiz");
+            System.out.println("-----------------------------------------------");
+            colaboracao = input.nextInt();
+            System.out.println("-----------------------------------------------");
+
+            break; // Sai do loop se a entrada for válida
+        } catch (InputMismatchException e) {
+            System.out.println("Entrada inválida. Digite um número inteiro.");
+            input.next(); // Limpa a entrada inválida
+        }
+
     //Finaliza o loop
-    } while (Colaboracao < 1 || Colaboracao > 3);
+    } while (colaboracao < 1 || colaboracao > 3);
     
+     //Calcula o FGTS de acordo com o cargo do funcionário
+    if (colaboracao == 1){
+        fgts = salarioBruto * 0.08f;
+     }
+    else if (colaboracao == 2){
+        fgts = salarioBruto * 0.112f;
+     }
+     else {
+        fgts = salarioBruto * 0.02f;
+     }
+
     /*
     * Cálculo do imposto de renda de acordo com a faixa salárial
     */
-    if (SalarioBruto <=2428.8) {
-      ImpostoDeRenda = 0;
+    if (salarioBruto <=2428.8) {
+      impostoDeRenda = 0;
      }
-    else if (SalarioBruto >=2428.81 && SalarioBruto <=2826.65){
-      ImpostoDeRenda = (SalarioBruto * 7.5f) / 100;
+    else if (salarioBruto >=2428.81 && salarioBruto <=2826.65){
+      impostoDeRenda = (salarioBruto * 7.5f) / 100;
      }  
-    else if (SalarioBruto >=2826.66 && SalarioBruto <=3751.05){
-      ImpostoDeRenda = SalarioBruto* 0.15f;
+    else if (salarioBruto >=2826.66 && salarioBruto <=3751.05){
+      impostoDeRenda = salarioBruto* 0.15f;
      }
-    else if (SalarioBruto >=3751.06 && SalarioBruto <=4664.68){
-      ImpostoDeRenda = SalarioBruto* 0.225f;
+    else if (salarioBruto >=3751.06 && salarioBruto <=4664.68){
+      impostoDeRenda = salarioBruto* 0.225f;
      }
     else {
-      ImpostoDeRenda = SalarioBruto*0.275f;
+      impostoDeRenda = salarioBruto*0.275f;
      }
     
     //Cálculo do salário recebido após descontos
-    SalarioLiquido = SalarioBruto - ImpostoDeRenda;
+    salarioLiquido = salarioBruto - impostoDeRenda;
 
     // Exibição dos resultados
-    System.out.println("Salário bruto: R$"+ SalarioBruto);
-    System.out.println("Salário liquido: R$"+ SalarioLiquido);
-    System.out.println("Valor descontado do Imposto de Renda: R$" + ImpostoDeRenda);
+    System.out.println("Salário bruto: R$"+ salarioBruto);
+    System.out.println("Salário liquido: R$"+ salarioLiquido);
+    System.out.println("Valor descontado do Imposto de Renda: R$" + impostoDeRenda);
     System.out.println("Valor descontado do INSS: R$");
     System.out.println("O valor do seu FGTS deve ser: R$" + fgts);
     
-    // Fecha o Scanner para liberar recursos
-    input.close();
+    input.close(); // Fecha o Scanner para liberar recursos
     }
-} 
+}
